26b5ec510a94910a4060a53a795c92bebd9069c8
ShaB <shab@cloud66.com>
2017-09-17 18:10:27 +0100

    Bitmap.set_colour

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -1,7 +1,7 @@
 class Bitmap
 	attr_reader :width, :height,:pixels
 
-	COLOR_WHITE = 'O'
+	COLOUR_WHITE = 'O'
 
 	def initialize(new_width, new_height)
 		raise 'Width should be a valid integer' unless new_width.is_a? Integer
@@ -13,11 +13,22 @@ class Bitmap
 
 		@width = new_width
 		@height = new_height
-		@pixels = Array.new(@height) {Array.new(@width,COLOR_WHITE)}
+		@pixels = Array.new(@height) {Array.new(@width, COLOUR_WHITE)}
 	end
 
 	def to_s
 		@pixels.inject('') {|result,row|  result + "#{row.join}\n" }.strip
 	end
 
+	def set_colour(x,y,new_colour)
+		raise 'X should be a valid integer' unless x.is_a? Integer
+		raise 'X coordinate should be between 1 and 250' unless x > 0 && x <= 250
+		raise "X coordinate should be less than width(#{@width})" if x > @width
+		raise 'Y should be a valid integer' unless y.is_a? Integer
+		raise 'Y coordinate should be between 1 and 250' unless y > 0 && y <= 250
+		raise "Y coordinate should be less than height(#{@height})" if y > @height
+		raise 'Colour should be a single capital letter' unless  new_colour.match(/[[:upper:]]/) && (new_colour.size == 1)
+
+		@pixels[y-1][x-1] = new_colour
+	end
 end
--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -3,7 +3,7 @@ require 'bitmap'
 describe Bitmap do
 
 	it 'has a COLOR_WHITE constant with value O' do
-		expect(Bitmap::COLOR_WHITE).to eq('O')
+		expect(Bitmap::COLOUR_WHITE).to eq('O')
 	end
 
 	describe '#initialize' do
@@ -78,7 +78,7 @@ describe Bitmap do
 			end
 
 			it 'should have a 10*20 two dimensional array as pixels all of them contains Bitmap::COLOR_WHITE' do
-				expect(@bitmap.pixels.all? {|row| row.all? {|pixel| pixel == Bitmap::COLOR_WHITE} }).to be true
+				expect(@bitmap.pixels.all? {|row| row.all? {|pixel| pixel == Bitmap::COLOUR_WHITE} }).to be true
 			end
 
 		end
@@ -97,6 +97,85 @@ describe Bitmap do
 				expect(Bitmap.new(2,2).to_s).to eq("OO\nOO")
 			end
 		end
+	end
+
+	describe '#set_colour' do
+
+		context  'With wrong arguments' do
+			it 'should raise ArgumentError with no arguments' do
+				expect {Bitmap.new(10,20).set_colour}.to raise_error(ArgumentError)
+			end
+
+			it 'should raise ArgumentError with one arguments' do
+				expect {Bitmap.new(10,20).set_colour(1)}.to raise_error(ArgumentError)
+			end
+
+			it 'should raise ArgumentError with two arguments' do
+				expect {Bitmap.new(10,20).set_colour(1,1)}.to raise_error(ArgumentError)
+			end
+
+			it 'should raise exception with wrong X type' do
+				expect {Bitmap.new(10,20).set_colour('1',1, 'C')}.to raise_error('X should be a valid integer')
+			end
+
+			it 'should raise exception with wrong Y type' do
+				expect {Bitmap.new(10,20).set_colour(1,'1', 'C')}.to raise_error('Y should be a valid integer')
+			end
+
+			it 'should raise exception with wrong colour type' do
+				expect {Bitmap.new(10,20).set_colour(1,1, 'c')}.to raise_error('Colour should be a single capital letter')
+			end
+
+			it 'should raise exception with wrong colour more than 1 character' do
+				expect {Bitmap.new(10,20).set_colour(1,1, 'CC')}.to raise_error('Colour should be a single capital letter')
+			end
+		end
+
+		context 'given x coordinate less than 1' do
+			it 'should raise exception' do
+				expect { Bitmap.new(10,20).set_colour(0,1,'C')}.to raise_error('X coordinate should be between 1 and 250')
+			end
+		end
+
+		context 'given x coordinate greater than 250' do
+			it 'should raise exception' do
+				expect { Bitmap.new(10,20).set_colour(251,1,'C')}.to raise_error('X coordinate should be between 1 and 250')
+			end
+		end
+
+		context 'given x coordinate greater than width' do
+			it 'should raise exception' do
+				expect { Bitmap.new(10,20).set_colour(20,1,'C')}.to raise_error('X coordinate should be less than width(10)')
+			end
+		end
+
+		context 'given y coordinate less than 1' do
+			it 'should raise exception' do
+				expect { Bitmap.new(10,20).set_colour(1,0,'C')}.to raise_error('Y coordinate should be between 1 and 250')
+			end
+		end
+
+		context 'given Y coordinate greater than 250' do
+			it 'should raise exception' do
+				expect { Bitmap.new(10,20).set_colour(1,251,'C')}.to raise_error('Y coordinate should be between 1 and 250')
+			end
+		end
+
+		context 'given y coordinate greater than height' do
+			it 'should raise exception' do
+				expect { Bitmap.new(10,10).set_colour(1,20,'C')}.to raise_error('Y coordinate should be less than height(10)')
+			end
+		end
+
+		context 'given x = 1 and y = 2 and colour C' do
+			it 'should set the colour of pixels[1,0] to C' do
+				bitmap = Bitmap.new(10,10)
+				expect(bitmap.pixels[1][0]).to eq(Bitmap::COLOUR_WHITE)
+				bitmap.set_colour(1,2,'C')
+				expect(bitmap.pixels[1][0]).to eq('C')
+			end
+		end
+
 	end
 
 end
