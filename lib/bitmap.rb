3d80ffdb42a4755741bc1519287e005b766ce845
ShaB <shab@cloud66.com>
2017-09-17 19:16:51 +0100

    Bitmap refactor

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -2,15 +2,11 @@ class Bitmap
 	attr_reader :width, :height,:pixels
 
 	COLOUR_WHITE = 'O'
+	MAX_LENGTH = 250
+	SIZE_PARAM_ERROR = "should be a valid integer between 1 and #{MAX_LENGTH}"
 
 	def initialize(new_width, new_height)
-		raise 'Width should be a valid integer' unless new_width.is_a? Integer
-		raise 'Height should be a valid integer' unless new_height.is_a? Integer
-		raise 'Width should be a positive integer' if  new_width < 0
-		raise 'Height should be a positive integer' if  new_height < 0
-		raise 'Width should be less than 250' if  new_width > 250
-		raise 'Height should be less than 250' if  new_height > 250
-
+		validate_initials(new_width, new_height)
 		@width = new_width
 		@height = new_height
 		@pixels = Array.new(@height) {Array.new(@width, COLOUR_WHITE)}
@@ -21,25 +17,34 @@ class Bitmap
 	end
 
 	def set_colour(x,y,new_colour)
-		raise 'X should be a valid integer' unless x.is_a? Integer
-		raise 'X coordinate should be between 1 and 250' unless x > 0 && x <= 250
-		raise "X coordinate should be less than width(#{@width})" if x > @width
-		raise 'Y should be a valid integer' unless y.is_a? Integer
-		raise 'Y coordinate should be between 1 and 250' unless y > 0 && y <= 250
-		raise "Y coordinate should be less than height(#{@height})" if y > @height
-		raise 'Colour should be a single capital letter' unless  new_colour.match(/[[:upper:]]/) && (new_colour.size == 1)
+		validate_coordinate(x,y)
+		raise ArgumentError.new('Colour should be a single capital letter') unless  new_colour.match(/[[:upper:]]/) && (new_colour.size == 1)
 
 		@pixels[y-1][x-1] = new_colour
 	end
 
 	def get_colour(x,y)
-		raise 'X should be a valid integer' unless x.is_a? Integer
-		raise 'X coordinate should be between 1 and 250' unless x > 0 && x <= 250
-		raise "X coordinate should be less than width(#{@width})" if x > @width
-		raise 'Y should be a valid integer' unless y.is_a? Integer
-		raise 'Y coordinate should be between 1 and 250' unless y > 0 && y <= 250
-		raise "Y coordinate should be less than height(#{@height})" if y > @height
+		validate_coordinate(x,y)
+
 		@pixels[y-1][x-1]
+	end
+
+	private
+
+	def valid_size_param?(param)
+		param.is_a?(Integer) && param > 0 && param <= MAX_LENGTH
+	end
+
+	def validate_initials(new_width, new_height)
+		raise ArgumentError.new("Width #{SIZE_PARAM_ERROR}") unless valid_size_param?new_width
+		raise ArgumentError.new("Height #{SIZE_PARAM_ERROR}") unless valid_size_param? new_height
+	end
+
+	def validate_coordinate(x,y)
+		raise ArgumentError.new("X #{SIZE_PARAM_ERROR}") unless valid_size_param?x
+		raise ArgumentError.new("X should be less than width(#{@width})") if x > @width
+		raise ArgumentError.new("Y #{SIZE_PARAM_ERROR}") unless valid_size_param? y
+		raise ArgumentError.new("Y should be less than height(#{@height})") if y > @height
 	end
 
 end
--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -6,6 +6,14 @@ describe Bitmap do
 		expect(Bitmap::COLOUR_WHITE).to eq('O')
 	end
 
+	it 'has a MAX_LENGTH constant with value 25O' do
+		expect(Bitmap::MAX_LENGTH).to eq(250)
+	end
+
+	it 'has a SIZE_PARAM_ERROR constant with value "should be a valid integer between 1 and MAX_LENGTH"' do
+		expect(Bitmap::SIZE_PARAM_ERROR).to eq("should be a valid integer between 1 and #{Bitmap::MAX_LENGTH}")
+	end
+
 	describe '#initialize' do
 		context  'With wrong initial values' do
 			it 'should raise ArgumentError with no initial argument' do
@@ -13,54 +21,48 @@ describe Bitmap do
 			end
 
 			it 'should raise ArgumentError with one initial argument' do
-				expect {Bitmap.new(0)}.to raise_error(ArgumentError)
+				expect {Bitmap.new(1)}.to raise_error(ArgumentError)
 			end
 
 			it 'should raise ArgumentError with one initial argument' do
-				expect {Bitmap.new(0)}.to raise_error(ArgumentError)
+				expect {Bitmap.new(1)}.to raise_error(ArgumentError)
 			end
 
 			it 'should raise exception with wrong initial width type' do
-				expect {Bitmap.new('0',0)}.to raise_error('Width should be a valid integer')
+				expect {Bitmap.new('1',1)}.to raise_error(ArgumentError,"Width #{Bitmap::SIZE_PARAM_ERROR}")
+			end
+
+			it 'should raise exception with zero width' do
+				expect {Bitmap.new(0,1)}.to raise_error(ArgumentError,"Width #{Bitmap::SIZE_PARAM_ERROR}")
+			end
+
+			it 'should raise exception with width more than Bitmap::MAX_LENGTH' do
+				expect {Bitmap.new(Bitmap::MAX_LENGTH + 1,1)}.to raise_error(ArgumentError,"Width #{Bitmap::SIZE_PARAM_ERROR}")
 			end
 
 			it 'should raise exception with wrong initial height type' do
-				expect {Bitmap.new(0,'0')}.to raise_error('Height should be a valid integer')
+				expect {Bitmap.new(1,'1')}.to raise_error(ArgumentError,"Height #{Bitmap::SIZE_PARAM_ERROR}")
 			end
 
-			it 'should raise exception with negative width' do
-				expect {Bitmap.new(-1,0)}.to raise_error('Width should be a positive integer')
+			it 'should raise exception with zero height' do
+				expect {Bitmap.new(1,0)}.to raise_error(ArgumentError,"Height #{Bitmap::SIZE_PARAM_ERROR}")
 			end
 
-			it 'should raise exception with width more than 250' do
-				expect {Bitmap.new(251,0)}.to raise_error('Width should be less than 250')
-			end
-
-			it 'should raise exception with negative height' do
-				expect {Bitmap.new(0,-1)}.to raise_error('Height should be a positive integer')
-			end
-
-			it 'should raise exception with height more than 250' do
-				expect {Bitmap.new(0,251)}.to raise_error('Height should be less than 250')
+			it 'should raise exception with height more than Bitmap::MAX_LENGTH' do
+				expect {Bitmap.new(1,Bitmap::MAX_LENGTH + 1)}.to raise_error(ArgumentError,"Height #{Bitmap::SIZE_PARAM_ERROR}")
 			end
 
 		end
 
 		context 'given 10 as width' do
 			it 'width should be 10' do
-				expect(Bitmap.new(10,0).width).to eq(10)
+				expect(Bitmap.new(10,1).width).to eq(10)
 			end
 		end
 
 		context 'given 10 as height' do
 			it 'height should be 10' do
-				expect(Bitmap.new(0,10).height).to eq(10)
-			end
-		end
-
-		context 'given 0 as width and 0 as height' do
-			it 'should have an empty array as pixels' do
-				expect(Bitmap.new(0,0).pixels).to eq([])
+				expect(Bitmap.new(1,10).height).to eq(10)
 			end
 		end
 
@@ -86,12 +88,6 @@ describe Bitmap do
 	end
 
 	describe '#to_s' do
-		context 'given 0 as width and 0 as height' do
-			it 'should be empty string'do
-				expect(Bitmap.new(0,0).to_s).to eq('')
-			end
-		end
-
 		context 'given 2 as width and 2 as height' do
 			it 'should return OO\nOO' do
 				expect(Bitmap.new(2,2).to_s).to eq("OO\nOO")
@@ -115,55 +111,55 @@ describe Bitmap do
 			end
 
 			it 'should raise exception with wrong X type' do
-				expect {Bitmap.new(10,20).set_colour('1',1, 'C')}.to raise_error('X should be a valid integer')
+				expect {Bitmap.new(10,20).set_colour('1',1, 'C')}.to raise_error(ArgumentError, "X #{Bitmap::SIZE_PARAM_ERROR}")
 			end
 
 			it 'should raise exception with wrong Y type' do
-				expect {Bitmap.new(10,20).set_colour(1,'1', 'C')}.to raise_error('Y should be a valid integer')
+				expect {Bitmap.new(10,20).set_colour(1,'1', 'C')}.to raise_error(ArgumentError, "Y #{Bitmap::SIZE_PARAM_ERROR}")
 			end
 
 			it 'should raise exception with wrong colour type' do
-				expect {Bitmap.new(10,20).set_colour(1,1, 'c')}.to raise_error('Colour should be a single capital letter')
+				expect {Bitmap.new(10,20).set_colour(1,1, 'c')}.to raise_error(ArgumentError, 'Colour should be a single capital letter')
 			end
 
 			it 'should raise exception with wrong colour more than 1 character' do
-				expect {Bitmap.new(10,20).set_colour(1,1, 'CC')}.to raise_error('Colour should be a single capital letter')
+				expect {Bitmap.new(10,20).set_colour(1,1, 'CC')}.to raise_error(ArgumentError, 'Colour should be a single capital letter')
 			end
 		end
 
 		context 'given x coordinate less than 1' do
 			it 'should raise exception' do
-				expect { Bitmap.new(10,20).set_colour(0,1,'C')}.to raise_error('X coordinate should be between 1 and 250')
+				expect { Bitmap.new(10,20).set_colour(0,1,'C')}.to raise_error(ArgumentError, "X #{Bitmap::SIZE_PARAM_ERROR}")
 			end
 		end
 
-		context 'given x coordinate greater than 250' do
+		context 'given x coordinate greater than Bitmap::MAX_LENGTH' do
 			it 'should raise exception' do
-				expect { Bitmap.new(10,20).set_colour(251,1,'C')}.to raise_error('X coordinate should be between 1 and 250')
+				expect { Bitmap.new(10,20).set_colour(Bitmap::MAX_LENGTH + 1,1,'C')}.to raise_error(ArgumentError, "X #{Bitmap::SIZE_PARAM_ERROR}")
 			end
 		end
 
 		context 'given x coordinate greater than width' do
 			it 'should raise exception' do
-				expect { Bitmap.new(10,20).set_colour(20,1,'C')}.to raise_error('X coordinate should be less than width(10)')
+				expect { Bitmap.new(10,20).set_colour(20,1,'C')}.to raise_error(ArgumentError, 'X should be less than width(10)')
 			end
 		end
 
 		context 'given y coordinate less than 1' do
 			it 'should raise exception' do
-				expect { Bitmap.new(10,20).set_colour(1,0,'C')}.to raise_error('Y coordinate should be between 1 and 250')
+				expect { Bitmap.new(10,20).set_colour(1,0,'C')}.to raise_error(ArgumentError, "Y #{Bitmap::SIZE_PARAM_ERROR}")
 			end
 		end
 
-		context 'given Y coordinate greater than 250' do
+		context 'given Y coordinate greater than Bitmap::MAX_LENGTH' do
 			it 'should raise exception' do
-				expect { Bitmap.new(10,20).set_colour(1,251,'C')}.to raise_error('Y coordinate should be between 1 and 250')
+				expect { Bitmap.new(10,20).set_colour(1,Bitmap::MAX_LENGTH + 1,'C')}.to raise_error(ArgumentError, "Y #{Bitmap::SIZE_PARAM_ERROR}")
 			end
 		end
 
 		context 'given y coordinate greater than height' do
 			it 'should raise exception' do
-				expect { Bitmap.new(10,10).set_colour(1,20,'C')}.to raise_error('Y coordinate should be less than height(10)')
+				expect { Bitmap.new(10,10).set_colour(1,20,'C')}.to raise_error(ArgumentError, 'Y should be less than height(10)')
 			end
 		end
 
@@ -188,24 +184,48 @@ describe Bitmap do
 			end
 
 			it 'should raise exception with wrong X type' do
-				expect {Bitmap.new(10,20).get_colour('1',1)}.to raise_error('X should be a valid integer')
+				expect {Bitmap.new(10,20).get_colour('1',1)}.to raise_error(ArgumentError, "X #{Bitmap::SIZE_PARAM_ERROR}")
 			end
 
 			it 'should raise exception with wrong Y type' do
-				expect {Bitmap.new(10,20).get_colour(1,'1')}.to raise_error('Y should be a valid integer')
+				expect {Bitmap.new(10,20).get_colour(1,'1')}.to raise_error(ArgumentError, "Y #{Bitmap::SIZE_PARAM_ERROR}")
 			end
 
 		end
 
+		context 'given x coordinate less than 1' do
+			it 'should raise exception' do
+				expect { Bitmap.new(10,20).get_colour(0,1)}.to raise_error(ArgumentError, "X #{Bitmap::SIZE_PARAM_ERROR}")
+			end
+		end
+
+		context 'given x coordinate greater than Bitmap::MAX_LENGTH' do
+			it 'should raise exception' do
+				expect { Bitmap.new(10,20).get_colour(Bitmap::MAX_LENGTH + 1,1)}.to raise_error(ArgumentError, "X #{Bitmap::SIZE_PARAM_ERROR}")
+			end
+		end
+
 		context 'given x coordinate greater than width' do
 			it 'should raise exception' do
-				expect { Bitmap.new(10,20).get_colour(20,1)}.to raise_error('X coordinate should be less than width(10)')
+				expect { Bitmap.new(10,20).get_colour(20,1)}.to raise_error(ArgumentError, 'X should be less than width(10)')
+			end
+		end
+
+		context 'given y coordinate less than 1' do
+			it 'should raise exception' do
+				expect { Bitmap.new(10,20).get_colour(1,0)}.to raise_error(ArgumentError, "Y #{Bitmap::SIZE_PARAM_ERROR}")
+			end
+		end
+
+		context 'given Y coordinate greater than Bitmap::MAX_LENGTH' do
+			it 'should raise exception' do
+				expect { Bitmap.new(10,20).get_colour(1,Bitmap::MAX_LENGTH + 1)}.to raise_error(ArgumentError, "Y #{Bitmap::SIZE_PARAM_ERROR}")
 			end
 		end
 
 		context 'given y coordinate greater than height' do
 			it 'should raise exception' do
-				expect { Bitmap.new(10,10).get_colour(1,20)}.to raise_error('Y coordinate should be less than height(10)')
+				expect { Bitmap.new(10,10).get_colour(1,20)}.to raise_error(ArgumentError, 'Y should be less than height(10)')
 			end
 		end
 
