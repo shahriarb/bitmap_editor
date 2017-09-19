eb259d466ad425af7a27797c815b74904a135e3e
ShaB <shab@cloud66.com>
2017-09-19 15:28:27 +0100

    CommandFactory.create

--- lib/command_factory.rb
+++ lib/command_factory.rb
@@ -1,3 +1,10 @@
+require 'initialize_command'
+require 'clear_command'
+require 'colour_command'
+require 'vline_command'
+require 'hline_command'
+require 'show_command'
+
 class CommandFactory
 	VALID_COMMANDS = [:initialize, :clear, :colour, :vline, :hline, :show]
 
@@ -11,6 +18,7 @@ class CommandFactory
 	}
 
 	INVALID_ABBR = 'abbreviation is not valid'
+	INVALID_COMMAND = 'command is not valid'
 
 	def self.abbr_to_command(abbreviation)
 		raise ArgumentError.new(INVALID_ABBR) unless abbreviation
@@ -19,5 +27,34 @@ class CommandFactory
 		raise ArgumentError.new(INVALID_ABBR) unless result
 
 		result
+	end
+
+	def self.create(command, *params)
+		case command
+			when :initialize
+				new_width = params[0].to_i rescue 0
+				new_height = params[1].to_i rescue 0
+				return InitializeCommand.new(new_width,new_height)
+			when :clear
+				return ClearCommand.new
+			when :colour
+				new_x = params[0].to_i rescue 0
+				new_y = params[1].to_i rescue 0
+				return ColourCommand.new(new_x,new_y,params[2])
+			when :vline
+				new_x = params[0].to_i rescue 0
+				new_y1 = params[1].to_i rescue 0
+				new_y2 = params[2].to_i rescue 0
+				return VlineCommand.new(new_x,new_y1,new_y2,params[3])
+			when :hline
+				new_x1 = params[0].to_i rescue 0
+				new_x2 = params[1].to_i rescue 0
+				new_y  = params[2].to_i rescue 0
+				return HlineCommand.new(new_x1,new_x2,new_y,params[3])
+			when :show
+				return ShowCommand.new
+			else
+				raise ArgumentError.new(INVALID_COMMAND)
+		end
 	end
 end
--- spec/command_factory_spec.rb
+++ spec/command_factory_spec.rb
@@ -65,8 +65,12 @@ describe CommandFactory do
 			expect(CommandFactory::COMMANDS_ABBR[:show]).to eq('S')
 		end
 
-		it 'has a constant array called INVALID_ABBR' do
+		it 'has a constant called INVALID_ABBR' do
 			expect(CommandFactory::INVALID_ABBR).to eq('abbreviation is not valid')
+		end
+
+		it 'has a constant called INVALID_COMMAND' do
+			expect(CommandFactory::INVALID_COMMAND).to eq('command is not valid')
 		end
 	end
 
@@ -126,6 +130,147 @@ describe CommandFactory do
 				expect {CommandFactory.abbr_to_command('M')}.to raise_error(ArgumentError,CommandFactory::INVALID_ABBR)
 			end
 		end
+	end
+
+	describe '.create' do
+		it 'should have at least one argument' do
+			expect {CommandFactory.create}.to raise_error(ArgumentError)
+		end
+
+		context 'given an invalid command' do
+			it 'should raise an exception' do
+				expect {CommandFactory.create(:some_command)}.to raise_error(ArgumentError,CommandFactory::INVALID_COMMAND)
+			end
+		end
+
+		context 'given :initialize as command' do
+			context 'given width=10 height=20' do
+				it 'should return InitializeCommand with correct attribute' do
+					result_command = CommandFactory.create(:initialize,10,20)
+					expect(result_command).to be_instance_of(InitializeCommand)
+					expect(result_command.width).to eq(10)
+					expect(result_command.height).to eq(20)
+				end
+			end
+
+			context 'given width=10 height=20 in string format' do
+				it 'should return InitializeCommand with correct attribute' do
+					result_command = CommandFactory.create(:initialize,'10','20')
+					expect(result_command).to be_instance_of(InitializeCommand)
+					expect(result_command.width).to eq(10)
+					expect(result_command.height).to eq(20)
+				end
+			end
+
+			context 'given wrong attributes' do
+				it 'should raise a proper exception' do
+					expect { CommandFactory.create(:initialize,0,20) }.to raise_error(ArgumentError, "Width #{BitmapUtils::SIZE_PARAM_ERROR}")
+				end
+			end
+		end
+
+
+		context 'given :clear as command' do
+			context 'no argument' do
+				it 'should return ClearCommand' do
+					expect(CommandFactory.create(:clear)).to be_instance_of(ClearCommand)
+				end
+			end
+		end
+
+		context 'given :colour as command' do
+			context 'given width=10 height=20 colour C' do
+				it 'should return ColourCommand with correct attribute' do
+					result_command = CommandFactory.create(:colour,10,20, 'C')
+					expect(result_command).to be_instance_of(ColourCommand)
+					expect(result_command.x).to eq(10)
+					expect(result_command.y).to eq(20)
+					expect(result_command.colour).to eq('C')
+				end
+			end
+
+			context 'given width=10 height=20 in string format' do
+				it 'should return ColourCommand with correct attribute' do
+					result_command = CommandFactory.create(:colour,'10','20', 'C')
+					expect(result_command).to be_instance_of(ColourCommand)
+					expect(result_command.x).to eq(10)
+					expect(result_command.y).to eq(20)
+				end
+			end
+
+			context 'given wrong attributes' do
+				it 'should raise a proper exception' do
+					expect { CommandFactory.create(:colour,0,20,'C') }.to raise_error(ArgumentError, "X #{BitmapUtils::SIZE_PARAM_ERROR}")
+				end
+			end
+		end
+
+		context 'given :vline as command' do
+			context 'given x=10 y1=5 y2=15 colour C' do
+				it 'should return VlineCommand with correct attribute' do
+					result_command = CommandFactory.create(:vline,10, 5, 15, 'C')
+					expect(result_command).to be_instance_of(VlineCommand)
+					expect(result_command.x).to eq(10)
+					expect(result_command.y1).to eq(5)
+					expect(result_command.y2).to eq(15)
+					expect(result_command.colour).to eq('C')
+				end
+			end
+
+			context 'given x=10 y1=5 y2=15 in string format' do
+				it 'should return VlineCommand with correct attribute' do
+					result_command = CommandFactory.create(:vline,'10', '5', '15', 'C')
+					expect(result_command).to be_instance_of(VlineCommand)
+					expect(result_command.x).to eq(10)
+					expect(result_command.y1).to eq(5)
+					expect(result_command.y2).to eq(15)
+				end
+			end
+
+			context 'given wrong attributes' do
+				it 'should raise a proper exception' do
+					expect { CommandFactory.create(:vline,0, 20, 30, 'C') }.to raise_error(ArgumentError, "X #{BitmapUtils::SIZE_PARAM_ERROR}")
+				end
+			end
+		end
+
+		context 'given :hline as command' do
+			context 'given x1=10 x2=15 y=5 colour C' do
+				it 'should return HlineCommand with correct attribute' do
+					result_command = CommandFactory.create(:hline,10, 15, 5, 'C')
+					expect(result_command).to be_instance_of(HlineCommand)
+					expect(result_command.x1).to eq(10)
+					expect(result_command.x2).to eq(15)
+					expect(result_command.y).to eq(5)
+					expect(result_command.colour).to eq('C')
+				end
+			end
+
+			context 'given x1=10 x2=15 y=5 colour C in string format' do
+				it 'should return HlineCommand with correct attribute' do
+					result_command = CommandFactory.create(:hline,'10', '15', '5', 'C')
+					expect(result_command).to be_instance_of(HlineCommand)
+					expect(result_command.x1).to eq(10)
+					expect(result_command.x2).to eq(15)
+					expect(result_command.y).to eq(5)
+				end
+			end
+
+			context 'given wrong attributes' do
+				it 'should raise a proper exception' do
+					expect { CommandFactory.create(:hline,0, 20, 30, 'C') }.to raise_error(ArgumentError, "X1 #{BitmapUtils::SIZE_PARAM_ERROR}")
+				end
+			end
+		end
+
+		context 'given :show as command' do
+			context 'no argument' do
+				it 'should return ShowCommand' do
+					expect(CommandFactory.create(:show)).to be_instance_of(ShowCommand)
+				end
+			end
+		end
+
 	end
 
 end
